03f12d270647132795b4ce8f8ee56ef37febc46a
JongeunKeum <jon9eun@gmail.com>
2022-08-15 20:19:09 +0900

    chapter13 - make component using composition

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Calculator from './chapter12/Calculator';
+import ProfileCard from './chapter13/ProfileCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <ProfileCard />
   </React.StrictMode>
 );
 
